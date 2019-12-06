330d97f973a1f296c44715dce3220dc0fba29b8d
Ilya Kuznetsov <i.g.kuznetsov@s7.ru>
2019-12-06 12:17:40 +0300

    add construct and destroy

--- 08/Vector.h
+++ 08/Vector.h
@@ -23,6 +23,16 @@ public:
     size_t max_size() const noexcept {
         return -1;
     }
+    
+    template<class U, class... Args>
+    void construct(U* p, Args&&... args) {
+        new((void *)p) U(std::forward<Args>(args)...);
+    }
+    
+    template<class U>
+    void destroy(U* p) {
+        p->~U();
+    }
 };
 
 
